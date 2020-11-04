9746f857b2141a615c105962cb53712e13c2250c
z5259729 <61221610+z5259729@users.noreply.github.com>
2020-11-05 02:13:19 +1100

    Registration
    New User is created in Firebase successfully

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Register.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Register.java
@@ -1,84 +1,84 @@
-package au.edu.unsw.infs3634.gamifiedlearning;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-public class Register extends AppCompatActivity {
-    EditText mName;
-    EditText mEmail;
-    EditText mPass;
-    Button mSignUp;
-    Button mGoToLogin;
-    FirebaseAuth fAuth;
-    ProgressBar status;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        mName = findViewById(R.id.etName);
-        mEmail = findViewById(R.id.etEmail);
-        mPass = findViewById(R.id.etPassword);
-        mSignUp = findViewById(R.id.btSignUp);
-        mGoToLogin = findViewById(R.id.btGoToLogIn);
-
-        fAuth = FirebaseAuth.getInstance();
-        status = findViewById(R.id.progressBar);
-
-        if(fAuth.getCurrentUser() !=null){
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-        }
-
-        mSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
-                String pass = mPass.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    mEmail.setError("Please enter an Email address!");
-                }
-
-                if (TextUtils.isEmpty(pass)) {
-                    mPass.setError("Please enter a valid Password");
-                }
-                // password length restriction can go here
-
-                status.setVisibility(View.VISIBLE);
-
-                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(Register.this, "Registered", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }else{
-
-                            Toast.makeText(Register.this, "Error!", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-            }
-
-        });
-
-    }
-}
+//package au.edu.unsw.infs3634.gamifiedlearning;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.text.TextUtils;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ProgressBar;
+//import android.widget.Toast;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//
+//public class Register extends AppCompatActivity {
+//    EditText mName;
+//    EditText mEmail;
+//    EditText mPass;
+//    Button mSignUp;
+//    Button mGoToLogin;
+//    FirebaseAuth fAuth;
+//    ProgressBar status;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_register);
+//
+//        mName = findViewById(R.id.etName);
+//        mEmail = findViewById(R.id.etEmail);
+//        mPass = findViewById(R.id.etPassword);
+//        mSignUp = findViewById(R.id.btSignUp);
+//        mGoToLogin = findViewById(R.id.btGoToLogIn);
+//
+//        fAuth = FirebaseAuth.getInstance();
+//        status = findViewById(R.id.progressBar);
+//
+//        if(fAuth.getCurrentUser() !=null){
+//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//            finish();
+//        }
+//
+//        mSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String email = mEmail.getText().toString().trim();
+//                String pass = mPass.getText().toString().trim();
+//
+//                if (TextUtils.isEmpty(email)) {
+//                    mEmail.setError("Please enter an Email address!");
+//                }
+//
+//                if (TextUtils.isEmpty(pass)) {
+//                    mPass.setError("Please enter a valid Password");
+//                }
+//                // password length restriction can go here
+//
+//                status.setVisibility(View.VISIBLE);
+//
+//                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful()) {
+//                            Toast.makeText(Register.this, "Registered", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//                        }else{
+//
+//                            Toast.makeText(Register.this, "Error!", Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    }
+//                });
+//            }
+//
+//        });
+//
+//    }
+//}
