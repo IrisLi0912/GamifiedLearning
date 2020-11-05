2b1292988718b53e66ceb125ec5063e568a0534f
z5259729 <61221610+z5259729@users.noreply.github.com>
2020-11-05 15:01:21 +1100

    Update Login.java

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Login.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Login.java
@@ -1,12 +1,20 @@
 package au.edu.unsw.infs3634.gamifiedlearning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -16,7 +24,7 @@ public class Login extends AppCompatActivity {
     Button mLogin;
     Button mGoToSignUp;
     FirebaseAuth fAuth;
-    ProgressBar status;
+    ProgressBar mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +35,41 @@ public class Login extends AppCompatActivity {
         mPass = findViewById(R.id.etLoginPassword);
         mLogin = findViewById(R.id.btLogIn);
         mGoToSignUp = findViewById(R.id.btGoLogIn);
+        fAuth = FirebaseAuth.getInstance();
+        mStatus = findViewById(R.id.loginProgressBar);
+
+        mLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Please enter an Email address!");
+                }
+
+                if (TextUtils.isEmpty(pass)) {
+                    mPass.setError("Please enter a valid Password");
+                }
+                // password length restriction can go here
+
+                mStatus.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Logged In", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Setting.class));
+                        } else {
+
+                            Toast.makeText(Login.this, "Error!", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
