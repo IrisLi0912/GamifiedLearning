96100ecaf0042a766f0cc76f6f190ddef6933f96
stigchen2333 <51014198+stigchen2333@users.noreply.github.com>
2020-11-05 19:00:27 +1100

    bottom navigation bar half finished

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Setting.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Setting.java
@@ -1,8 +1,14 @@
 package au.edu.unsw.infs3634.gamifiedlearning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Setting extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        //initialise and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bnSetting);
+
+        //set home selected, later change to set whatever page selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.dashboard:
+                        startActivity(new Intent(getApplicationContext(),Setting.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about:
+                        startActivity(new Intent(getApplicationContext(),User.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainPage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+
+                }
+                return false;
+            }
+        });
     }
 }
