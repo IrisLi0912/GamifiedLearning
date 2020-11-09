20d30fc7c4ced2aee73e72b18f97b313c99ed543
stigchen2333 <51014198+stigchen2333@users.noreply.github.com>
2020-11-09 16:19:22 +1100

    timer added. 30s for each question.

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizActivity.java
@@ -2,9 +2,11 @@ package au.edu.unsw.infs3634.gamifiedlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -15,8 +17,12 @@ import android.widget.Toast;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
+    public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000; //this is the timer for the quiz
+
     private TextView textViewQuestion;
     private TextView textViewScore;
     private TextView textViewQuestionCount;
@@ -27,13 +33,20 @@ public class QuizActivity extends AppCompatActivity {
     private RadioButton rb3;
     private Button buttonConfirmNext;
 
-    private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultRb;//rb for radio button
+    private ColorStateList textColorDefaultCd; //cd for countdown
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
     private List<Question> questionList;
     private int questionCounter;
     private int questionCountTotal;
     private Question currentQuestion;
     private int score;
     private boolean answered;
+
+    private long backPressedTime;
 
 
     @Override
@@ -52,6 +65,7 @@ public class QuizActivity extends AppCompatActivity {
         buttonConfirmNext = findViewById(R.id.button_confirm_next);
 
         textColorDefaultRb = rb1.getTextColors(); //get text clolr, green or red
+        textColorDefaultCd = textViewCountDown.getTextColors();
 
         QuizDbHelper dbHelper = new QuizDbHelper(this);
         questionList = dbHelper.getAllQuestions(); //fill the list with data
@@ -94,13 +108,50 @@ public class QuizActivity extends AppCompatActivity {
             textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
             answered = false;
             buttonConfirmNext.setText("Confirm");
+
+            //we also want to start the countdown here. Can be moved to somewhere else
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
         } else {
             finishQuiz();
+        }
+    }
+    private void startCountDown() {
+        //this starts the countdown.
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        textViewCountDown.setText(timeFormatted);
+        if (timeLeftInMillis < 10000) {
+            //warning the user by changing the timer color
+            textViewCountDown.setTextColor(Color.RED);
+        } else {
+            textViewCountDown.setTextColor(textColorDefaultCd);
         }
     }
 
     private void checkAnswer() {
         answered = true;
+
+        countDownTimer.cancel();
+
+
         RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
         int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
         if (answerNr == currentQuestion.getAnswerNr()) {
@@ -137,8 +188,32 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void finishQuiz() {
+        //we sent the result to another activity
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
         //maybe we put a method that update the score into the firebase or leader board
         finish();
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        //a method that stop user accidentally press back button and go back to the previous page
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finishQuiz();
+        } else {
+            Toast.makeText(this, "Press back again to finish", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            //stop the timer or it will be keep running at the back
+        }
     }
 }
--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/StartingScreenActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/StartingScreenActivity.java
@@ -3,16 +3,28 @@ package au.edu.unsw.infs3634.gamifiedlearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingScreenActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1; //value here dose not matter
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighscore";
+    private TextView textViewHighscore; //reference the textview and show the high score
+    private int highscore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        textViewHighscore=findViewById(R.id.text_view_highscore);
+        loadHighscore();
 
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
         buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +37,35 @@ public class StartingScreenActivity extends AppCompatActivity {
     }
     private void startQuiz(){
         Intent intent = new Intent (StartingScreenActivity.this, QuizActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //displaying the HIGHEST score
+        if (requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highscore) {
+                    updateHighscore(score);
+                }
+            }
+        }
+    }
+    private void loadHighscore() {
+        //modify needed, fetch high score from firebase?
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("Highscore: " + highscore);
+    }
+
+    private void updateHighscore(int highscoreNew) {
+        highscore = highscoreNew;
+        textViewHighscore.setText("Highscore: " + highscore);
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
     }
 }
