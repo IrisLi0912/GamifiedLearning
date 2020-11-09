4d5f232cc5238ea41da812ec08988b6f4b529607
stigchen2333 <51014198+stigchen2333@users.noreply.github.com>
2020-11-09 16:40:16 +1100

    basic template done

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Question.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/Question.java
@@ -1,6 +1,9 @@
 package au.edu.unsw.infs3634.gamifiedlearning;
 
-public class Question {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Question implements Parcelable {
     private String question;
     private String option1;
     private String option2;
@@ -19,6 +22,26 @@ public class Question {
         this.answerNr = answerNr;
         //nr means number, this is storing the number of the correct answer
     }
+
+    protected Question(Parcel in) {
+        question = in.readString();
+        option1 = in.readString();
+        option2 = in.readString();
+        option3 = in.readString();
+        answerNr = in.readInt();
+    }
+
+    public static final Creator<Question> CREATOR = new Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
 
     public String getQuestion() {
         return question;
@@ -58,5 +81,19 @@ public class Question {
 
     public void setAnswerNr(int answerNr) {
         this.answerNr = answerNr;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(question);
+        dest.writeString(option1);
+        dest.writeString(option2);
+        dest.writeString(option3);
+        dest.writeInt(answerNr);
     }
 }
--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizActivity.java
@@ -14,6 +14,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +23,12 @@ import java.util.Locale;
 public class QuizActivity extends AppCompatActivity {
     public static final String EXTRA_SCORE = "extraScore";
     private static final long COUNTDOWN_IN_MILLIS = 30000; //this is the timer for the quiz
+
+    private static final String KEY_SCORE = "keyScore";
+    private static final String KEY_QUESTION_COUNT = "keyQuestionCount";
+    private static final String KEY_MILLIS_LEFT = "keyMillisLeft";
+    private static final String KEY_ANSWERED = "keyAnswered";
+    private static final String KEY_QUESTION_LIST = "keyQuestionList";
 
     private TextView textViewQuestion;
     private TextView textViewScore;
@@ -39,7 +46,7 @@ public class QuizActivity extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private long timeLeftInMillis;
 
-    private List<Question> questionList;
+    private ArrayList<Question> questionList;
     private int questionCounter;
     private int questionCountTotal;
     private Question currentQuestion;
@@ -66,13 +73,32 @@ public class QuizActivity extends AppCompatActivity {
 
         textColorDefaultRb = rb1.getTextColors(); //get text clolr, green or red
         textColorDefaultCd = textViewCountDown.getTextColors();
+        //the below if else statement:
+        //only want do this if we do not have a instant saved. the orientation problem
+        if (savedInstanceState == null) {
+            QuizDbHelper dbHelper = new QuizDbHelper(this);
+            questionList = dbHelper.getAllQuestions(); //fill the list with data
+            questionCountTotal = questionList.size();
+            Collections.shuffle(questionList);
 
-        QuizDbHelper dbHelper = new QuizDbHelper(this);
-        questionList = dbHelper.getAllQuestions(); //fill the list with data
-        questionCountTotal = questionList.size();
-        Collections.shuffle(questionList);
+            showNextQuestion();
+        } else {
+            //restore previous state from parcelable array list
+            questionList = savedInstanceState.getParcelableArrayList(KEY_QUESTION_LIST);
+            questionCountTotal = questionList.size();
+            questionCounter = savedInstanceState.getInt(KEY_QUESTION_COUNT);
+            currentQuestion = questionList.get(questionCounter - 1);
+            score = savedInstanceState.getInt(KEY_SCORE);
+            timeLeftInMillis = savedInstanceState.getLong(KEY_MILLIS_LEFT);
+            answered = savedInstanceState.getBoolean(KEY_ANSWERED);
+            if (!answered) {
+                startCountDown();
+            } else {
+                updateCountDownText();
+                showSolution();
+            }
+        }
 
-        showNextQuestion();
 
         buttonConfirmNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,6 +142,7 @@ public class QuizActivity extends AppCompatActivity {
             finishQuiz();
         }
     }
+
     private void startCountDown() {
         //this starts the countdown.
         countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
@@ -133,6 +160,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         }.start();
     }
+
     private void updateCountDownText() {
         int minutes = (int) (timeLeftInMillis / 1000) / 60;
         int seconds = (int) (timeLeftInMillis / 1000) % 60;
@@ -208,6 +236,7 @@ public class QuizActivity extends AppCompatActivity {
         }
         backPressedTime = System.currentTimeMillis();
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -215,5 +244,16 @@ public class QuizActivity extends AppCompatActivity {
             countDownTimer.cancel();
             //stop the timer or it will be keep running at the back
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //fixing the orientation change problem
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SCORE, score);
+        outState.putInt(KEY_QUESTION_COUNT, questionCounter);
+        outState.putLong(KEY_MILLIS_LEFT, timeLeftInMillis);
+        outState.putBoolean(KEY_ANSWERED, answered);
+        outState.putParcelableArrayList(KEY_QUESTION_LIST, questionList);
     }
 }
