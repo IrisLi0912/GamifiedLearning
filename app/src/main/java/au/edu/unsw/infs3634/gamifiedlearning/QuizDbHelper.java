7427ee7e47d097ac995d874b813a88dbe3a012ee
stigchen2333 <51014198+stigchen2333@users.noreply.github.com>
2020-11-14 12:24:07 +1100

    the basic funtion is done. Data base is populated

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizDbHelper.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuizDbHelper.java
@@ -56,51 +56,55 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         addQuestion(q1);
         Question q2 = new Question("Calculate the gravitational potential energy of the entire Earth while it orbits the Sun, given that the Sun has a mass of 2x10^30 kg and the distance of the Earth to the Sun is 1.5x10^11 km.","20*10^22 Joule", "3.56x10^22 Joule", "More information is required for calculation.", 2, Question.CHAPTER_1);
         addQuestion(q2);
-        Question q3 = new Question("What is the g-force experienced in a stable orbit","1", "-9.8", "0", 3, Question.CHAPTER_2);
+        Question q3 = new Question("What is the g-force experienced in a stable orbit","1", "-9.8", "0", 3, Question.CHAPTER_1);
         addQuestion(q3);
-        Question q4 = new Question("Hard: A is correct","A", "B", "C", 1, Question.CHAPTER_3);
+        Question q4 = new Question("What was the result of the Michelson–Morley experiment?","Michelson and Morley failed to observe any change in the interference pattern.", "Prove that ether exists", "Determine the accurate speed of light is constant in the same frame of reference. ", 1, Question.CHAPTER_1);
         addQuestion(q4);
-        Question q5 = new Question("Hard: B is correct","A", "B", "C", 2, Question.CHAPTER_3);
+        Question q5 = new Question("A satellite orbits Earth with an elliptical orbit, when dose it have the greatest kinetic energy?","Furthest to the Earth", "Closest to the Earth", "The kinetic energy stays the same", 2, Question.CHAPTER_1);
         addQuestion(q5);
-        Question q6 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+
+        Question q6 = new Question("What is the radius of Mars?","1731.1 km?", "2570 KM", "3390 KM", 3, Question.CHAPTER_2);
         addQuestion(q6);
-        Question q7 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q7 = new Question("How long is one day on Mars?","24 hours", "24.6 hours", "18.2 hours", 2, Question.CHAPTER_2);
         addQuestion(q7);
-        Question q8 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q8 = new Question("What is the highest temperature on Mars?","20 degree Celsius", "-8 degree Celsius", "5 degree Celsius", 1, Question.CHAPTER_2);
         addQuestion(q8);
-        Question q9 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q9 = new Question("Why is understanding the climate on Mars important?","To understand if the Mars will turn into a supernova", "Prepare for Elon Musk's landing", "Understanding the climate of Mars is important to facilitate human exploration and to determine if Mars could have the conditions to support life", 3, Question.CHAPTER_2);
         addQuestion(q9);
-        Question q10 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q10 = new Question("What had been discovered from the inside of the Martian Meteorite?","Huge amount of Carbon 14", "tiny gas bubbles, water and organic (carbon-based) compounds in them.", "Only silicon dioxide ", 2, Question.CHAPTER_2);
         addQuestion(q10);
-        Question q11 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+
+        Question q11 = new Question("When was Aristotle first observed an occultation of Mars?","A", "B", "4 May 357 BCE", 3, Question.CHAPTER_3);
         addQuestion(q11);
-        Question q12 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q12 = new Question("When was the first standard nomenclature for Martian albedo features was introduced?","1906", "1900", "1960", 3, Question.CHAPTER_3);
         addQuestion(q12);
-        Question q13 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q13 = new Question("How was the amount of oxygen and water vapor in the atmosphere of Mars measured?","By measuring the weight of the soil relative to the density", "By measuring spectral lines that were redshifted by the orbital motions of Mars and Earth", "By pressurising the air in a chamber and measure the density change", 2, Question.CHAPTER_3);
         addQuestion(q13);
-        Question q14 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q14 = new Question("How was the thermal energy being radiated by the surface of Mars measured?","Using a reflective telescope attached on the ISS", "Using a vacuum thermocouple attached to the 2.54 m (100 in) Hooker Telescope", "By measuring the temperature change on Mar's surface.", 2, Question.CHAPTER_3);
         addQuestion(q14);
-        Question q15 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q15 = new Question("When was The Working Group for Planetary System Nomenclature (WGPSN) established?","1973", "1994", "2001", 1, Question.CHAPTER_3);
         addQuestion(q15);
-        Question q16= new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+
+        Question q16= new Question("When will the “Perseverance” be landed?","Sep. 26, 2022", "May. 31, 2025", "Feb. 18, 2021", 3, Question.CHAPTER_4);
         addQuestion(q16);
-        Question q17 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q17 = new Question("How long is the expected mission duration?","At least 7 Mars year", "36 days Mars day", "At least one Mars year", 3, Question.CHAPTER_4);
         addQuestion(q17);
-        Question q18 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q18 = new Question("What fuel is used to provide power for the “Persercerance”?","carbon-14", "plutonium-238", "Diesel", 2, Question.CHAPTER_4);
         addQuestion(q18);
-        Question q19 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q19 = new Question("When will/ was the “Perseverance” launched?","Feb. 18, 2022", "July 30, 2020", "C", 2, Question.CHAPTER_4);
         addQuestion(q19);
-        Question q20 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q20 = new Question("How many cameras are on the “Persercerance”?","19", "B", "C", 1, Question.CHAPTER_4);
         addQuestion(q20);
-        Question q21 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+
+        Question q21 = new Question("How high is Olympus Mons - the tallest mountain - in the solar system?","190 KM", "77 KM", "21 KM", 3, Question.CHAPTER_5);
         addQuestion(q21);
-        Question q22 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q22 = new Question("What color is the sunset on Mars?","Yellow", "Same as mars", "Blue", 3, Question.CHAPTER_5);
         addQuestion(q22);
-        Question q23 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q23 = new Question("How many missions to Mars have been successful (Until 2020)","27", "18", "5", 2, Question.CHAPTER_5);
         addQuestion(q23);
-        Question q24 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q24 = new Question("Which Planet has the largest dust storms in the solar system?","Jupiter", "Mars", "Earth", 2, Question.CHAPTER_5);
         addQuestion(q24);
-        Question q25 = new Question("Hard: C is correct","A", "B", "C", 3, Question.CHAPTER_3);
+        Question q25 = new Question("What is Mar's surface gravity relative to the Earth?","37% of the Earth", "10% of the Earth", "1.3 times of the Earth", 1, Question.CHAPTER_5);
         addQuestion(q25);
 
     }
--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/StartingScreenActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/StartingScreenActivity.java
@@ -16,13 +16,16 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static au.edu.unsw.infs3634.gamifiedlearning.Question.CHAPTER_1;
+import static au.edu.unsw.infs3634.gamifiedlearning.Question.CHAPTER_2;
+import static au.edu.unsw.infs3634.gamifiedlearning.Question.CHAPTER_3;
+import static au.edu.unsw.infs3634.gamifiedlearning.Question.CHAPTER_4;
+import static au.edu.unsw.infs3634.gamifiedlearning.Question.CHAPTER_5;
 
 public class StartingScreenActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_QUIZ = 1; //value here dose not matter
     public static final String EXTRA_DIFFICULTY = "extraDifficulty";
-
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String KEY_HIGHSCORE = "keyHighscore";
+    private static final int REQUEST_CODE_QUIZ = 1; //value here dose not matter
     private TextView textViewHighscore; //reference the textview and show the high score
     private int highscore;
     private Spinner spinnerDifficulty;
@@ -43,18 +46,18 @@ public class StartingScreenActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.topic:
-                        startActivity(new Intent(getApplicationContext(),MainTopicMain.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), MainTopicMain.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(),User.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), User.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.home:
-                        startActivity(new Intent(getApplicationContext(),MainPage.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), MainPage.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.journey:
                         startActivity(new Intent(getApplicationContext(), StartingScreenActivity.class));
@@ -69,50 +72,108 @@ public class StartingScreenActivity extends AppCompatActivity {
         //below is the spinner stuff
         spinnerDifficulty = findViewById(R.id.spinner_difficulty);
         //calling the difficulty list in question class
-        String [] difficultyLevels = Question.getAllDifficultyLevels();
+        String[] difficultyLevels = Question.getAllDifficultyLevels();
         ArrayAdapter<String> adapterDifficulty = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, difficultyLevels);
         adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDifficulty.setAdapter(adapterDifficulty);
         //spinner end
 
-        textViewHighscore=findViewById(R.id.text_view_highscore);
+        textViewHighscore = findViewById(R.id.text_view_highscore);
         loadHighscore();
 
-        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);//Temporarily hided from app interface
+        Button buttonStartQuiz1 = findViewById(R.id.btChapter1);
+        Button buttonStartQuiz2 = findViewById(R.id.btChapter2);
+        Button buttonStartQuiz3 = findViewById(R.id.btChapter3);
+        Button buttonStartQuiz4 = findViewById(R.id.btChapter4);
+        Button buttonStartQuiz5 = findViewById(R.id.btChapter5);
+
+        //------------------Temporarily hided from app interface ------------------
         buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startQuiz();
             }
         });
-
-        Button buttonStartQuiz1 = findViewById(R.id.btChapter1);
+        //-------------------------------------------------------------------------
         buttonStartQuiz1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startQuizChapter1();
             }
         });
-
-
+        buttonStartQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuizChapter2();
+            }
+        });
+        buttonStartQuiz3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuizChapter3();
+            }
+        });
+        buttonStartQuiz4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuizChapter4();
+            }
+        });
+        buttonStartQuiz5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuizChapter5();
+            }
+        });
     }
 
     private void startQuizChapter1() {
-        Intent intent = new Intent (StartingScreenActivity.this, QuizActivity.class);
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
         //send the value to quiz activity
         intent.putExtra(EXTRA_DIFFICULTY, CHAPTER_1);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
 
-    private void startQuiz(){
-        //select and record the difficulty selection
-        String difficulty = spinnerDifficulty.getSelectedItem().toString();
-        Intent intent = new Intent (StartingScreenActivity.this, QuizActivity.class);
+    private void startQuizChapter2() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
         //send the value to quiz activity
-        intent.putExtra(EXTRA_DIFFICULTY,difficulty);
+        intent.putExtra(EXTRA_DIFFICULTY, CHAPTER_2);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
+
+    private void startQuizChapter3() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        //send the value to quiz activity
+        intent.putExtra(EXTRA_DIFFICULTY, CHAPTER_3);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    private void startQuizChapter4() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        //send the value to quiz activity
+        intent.putExtra(EXTRA_DIFFICULTY, CHAPTER_4);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    private void startQuizChapter5() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        //send the value to quiz activity
+        intent.putExtra(EXTRA_DIFFICULTY, CHAPTER_5);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    //------------------Temporarily hided from app interface ------------------
+    private void startQuiz() {
+        //select and record the difficulty selection
+        String difficulty = spinnerDifficulty.getSelectedItem().toString();
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        //send the value to quiz activity
+        intent.putExtra(EXTRA_DIFFICULTY, difficulty);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+    //-------------------------------------------------------------------------
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -126,6 +187,7 @@ public class StartingScreenActivity extends AppCompatActivity {
             }
         }
     }
+
     private void loadHighscore() {
         //modify needed, fetch high score from firebase?
 
