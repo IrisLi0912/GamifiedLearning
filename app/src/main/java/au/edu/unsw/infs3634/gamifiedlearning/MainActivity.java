d37376c025e2a6ac75b77d201c2287cb55a05e91
z5259729 <61221610+z5259729@users.noreply.github.com>
2020-11-16 14:53:30 +1100

    Firebase User Data Storing

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +15,15 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText mName;
@@ -24,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
     Button mSignUp;
     Button mGoToLogin;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
     ProgressBar status;
+
+    public static final String TAG = "TAG";
 
 
     @Override
@@ -37,9 +49,11 @@ public class MainActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.etEmail);
         mPass = findViewById(R.id.etPassword);
         mSignUp = findViewById(R.id.btSignUp);
+        //mUserName = findViewById(R.id.etUserName);
         mGoToLogin = findViewById(R.id.btGoLogIn);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         status = findViewById(R.id.progressBar);
 
         if(fAuth.getCurrentUser() !=null){
@@ -53,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPass.getText().toString().trim();
+                String name = mName.getText().toString();
+               // String userName = mUserName.gettext.toString.trim();
+
+
 
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Please enter an Email address!");
@@ -65,12 +83,26 @@ public class MainActivity extends AppCompatActivity {
 
                 status.setVisibility(View.VISIBLE);
 
-                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             Toast.makeText(MainActivity.this, "Registered", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), Setting.class));
+
+                            // Stores User Data to FireStore
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference dR = fStore.collection("users").document(userID);
+                            HashMap<String, Object> user = new HashMap<>();
+                            user.put("name", name);
+                            user.put("email", email);
+                           // user.put("userName", userName);
+                            dR.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user created" + userID);
+                                }
+                            });
+                        startActivity(new Intent(getApplicationContext(), Setting.class));
                             //Currently redirects to setting screen to test logout functionality
                         }else{
 
